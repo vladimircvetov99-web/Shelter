8c7fbeb214db31ce62c586e1ddbdcae0ca810fee
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-05-12 15:37:31 +0300

    Finish the adaptive version and fix scrolling on all pages

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
+const body = document.querySelector("body");
+const navItems = document.querySelectorAll('nav .nav__list a');
 
-burger.addEventListener("click", () => {
+const BURGER_MENU_SHOW_WIDTH = 768;
+
+function toggleMenu() {
+  if (window.innerWidth > BURGER_MENU_SHOW_WIDTH) {
+    return;
+  }
+
   burger.classList.toggle("open");
   nav.classList.toggle("open");
+  body.style.overflow = opened ? '' : 'hidden';
+  opened = !opened;
+}
+
+let opened = false;
+burger.addEventListener("click", toggleMenu);
+
+[...navItems].forEach((element) => {
+  element.addEventListener("click", toggleMenu);
 });
+
+
